08d05a76703a7232f5b83bcfe0f34326c918c575
Zonglei Jing <jingzonglei@163.com>
2021-08-30 23:34:04 +0800

    banner

--- volunteer/src/views/Home.vue
+++ volunteer/src/views/Home.vue
@@ -1,26 +1,47 @@
 <template>
+  <!-- 顶部菜单 -->
   <Header/>
-  <!-- <div class="home">
-    
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div> -->
+<!--  轮播图 -->
+  <Swiper v-if="banners.length > 0">
+    <Slide v-for="(item,index) in banners" :key="index">
+      <img :src="item.src" alt="" class="swiper-img"/>
+    </Slide>
+  </Swiper>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "@/components/Header";
+import { Swiper, Slide } from 'vue-swiper-component';
 
 export default {
   name: 'Home',
   components: {
-    Header
-  }
+    Header,
+    Swiper,
+    Slide
+  },
+  data() {
+    return {
+      banners: [{
+        src: '/images/demo/banner1.jpeg'
+      }, {
+        src: '/images/demo/banner2.jpeg'
+      }, {
+        src: '/images/demo/banner3.jpeg'
+      }, {
+        src: '/images/demo/banner4.jpeg'
+      }]
+    }
+  },
 }
 </script>
 
 <style scoped>
 .home {
   width: 100%;
+}
+.swiper-img {
+  height: 400px;
 }
 </style>
